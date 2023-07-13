3dbc233888e90e74187e3fa18bf7fb4c5a4de2e0
BrentGruber <brent.gruber77@gmail.com>
2023-07-12 22:41:13 -0400

    adding tailscale install script

---
+++ shell/install-tailscale.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Add the tailscale signing key and repository
+curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/jammy.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
+curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/jammy.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
+
+
+# Install tailscale
+sudo apt-get update && sudo apt-get install -y tailscale

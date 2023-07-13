3b6941626117c45c022fea3f3f8df26584a856bc
BrentGruber <brent.gruber77@gmail.com>
2023-07-12 22:34:35 -0400

    adding global scope to setup doppler script

--- shell/setup-doppler-cli.sh
+++ shell/setup-doppler-cli.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+# NOTE: This script assumes that the doppler cli is already authenticated
+
+
 # Setup doppler based on the host name
 # This does a root setup, so secrets will be available
 # from any directory
-doppler setup -p infrastructure -c linux_$(hostname)
+doppler setup -p infrastructure -c linux_$(hostname) --scope /
